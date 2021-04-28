69fb1e9278cc874fcace0060e0dcd0f2e6115503
Kaliszando <kaliszadam99@gmail.com>
2021-04-28 20:56:25 +0200

    base project

---
+++ src/main/java/com/challenges/App.java
@@ -0,0 +1,8 @@
+package com.challenges;
+
+public class App {
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/challenges/AppTest.java
@@ -0,0 +1,13 @@
+package com.challenges;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
